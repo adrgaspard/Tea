6d5886a8b7dbaba77e3bcfb4f6d97e46b2f86b81
Gaspard <adr.gaspard@gmail.com>
2023-06-16 01:53:02 +0200

    [CommonTools] Edit ErrorException

--- AdrGaspard.Tea.CommonTools/ErrorException.cs
+++ AdrGaspard.Tea.CommonTools/ErrorException.cs
@@ -2,5 +2,10 @@
 {
     public abstract class ErrorException : Exception
     {
+        protected ErrorException() { }
+
+        protected ErrorException(string? message) : base(message) { }
+
+        protected ErrorException(string? message, Exception? innerException) : base(message, innerException) { }
     }
 }
